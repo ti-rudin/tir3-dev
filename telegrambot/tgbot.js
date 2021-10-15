6eec08f7478b94aabbbfa246cd441e646bc43e5e
Alexander Rudin <ax.rudin@gmail.com>
2021-10-15 23:37:40 +0000

    leads in tg

--- telegrambot/tgbot.js
+++ telegrambot/tgbot.js
@@ -11,17 +11,21 @@ const chatid = process.env.CHATID;
 const userid = process.env.GOOGLEUID;
 
 if (tgkey === undefined) {
+
   throw new TypeError('BOT_TOKEN must be provided!')
+
 }
 const bot = new TelegramBot(tgkey, { polling: true });
 
 function send(tgpost) {
+
   bot.sendMessage(chatid, tgpost);
+
 }
 
-let leads;
 function sendlead(botname, profit, duration, moneta) {
-  let tgpost = '<b>' + botname + '</b> \nПрибыль: <pre>' + profit.toFixed(2) + ' ' + moneta +'</pre>\nДлительность: ' + duration;
+
+  let tgpost = '<b>' + botname + '</b> \nПрибыль: <pre>' + profit.toFixed(2) + ' ' + moneta + '</pre>\nДлительность: ' + duration;
   bot.sendMessage(chatid, tgpost, { parse_mode: "HTML" });
 
 }
@@ -29,7 +33,6 @@ function sendlead(botname, profit, duration, moneta) {
 var subscriber = redis.createClient("//redis:6379");
 //var subscriber = redis.createClient("//localhost:6379");
 subscriber.auth("YzRAdGgkFg");
-
 
 subscriber.on("message", function (channel, message) {
   let msgg = JSON.parse(message);
@@ -52,8 +55,35 @@ subscriber.on("message", function (channel, message) {
 
 subscriber.subscribe("tgpost");
 
+let leads;
 bot.on('message', (msg) => {
-  bot.sendMessage(msg.chat.id, "CHAT ID: " + msg.chat.id);
+
+  if ((msg.text == 'id') || (msg.text == 'Id') || (msg.text == 'ID')) {
+    bot.sendMessage(msg.chat.id, "CHAT ID: " + msg.chat.id);
+
+  } else {
+
+    client.get(userid + "-leads", function (err, reply) {
+
+      leads = JSON.parse(reply);
+      let count = leads.length;
+      var sum = 0;
+
+      for (var i = 0; i < leads.length; i++) {
+
+        let lead = leads[i];
+        sum = sum + lead.profit;
+
+      }
+
+      let sr = sum / count;
+
+      bot.sendMessage(msg.chat.id, "Сделок: <b>" + count + "</b>\nСумма: <pre>" + sum.toFixed(2) + "</pre>\nСреднее: <b>" + sr.toFixed(2) + "</b>", { parse_mode: "HTML" });
+
+    });
+
+  }
+
 });
 
 // Enable graceful stop
